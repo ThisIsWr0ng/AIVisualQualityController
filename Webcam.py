c46485e5fdea510cb662ad46074faaa0d6641141
ThisIsWr0ng <dawidolesko21@gmail.com>
2022-12-23 13:49:41 +0000

    added webcam.py

---
+++ Webcam.py
@@ -0,0 +1,23 @@
+import cv2
+
+cv2.namedWindow("preview")
+url = "https://192.168.1.239:8080/video"
+webcam = 0 # 0 for laptop webcam / 1 for external webcam
+vc = cv2.VideoCapture(url)
+
+if vc.isOpened(): # try to get the first frame
+    rval, frame = vc.read()
+else:
+    vc = cv2.VideoCapture(webcam)
+    rval, frame = vc.read()
+    #rval = False
+
+while rval:
+    cv2.imshow("preview", frame)
+    rval, frame = vc.read()
+    key = cv2.waitKey(20)
+    if key == 27: # exit on ESC
+        break
+
+vc.release()
+cv2.destroyWindow("preview")
