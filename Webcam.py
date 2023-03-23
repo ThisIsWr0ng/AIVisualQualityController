ddfce9b04fd0405dd6fe7718064e00f558f2feaf
Dawid Olesko <dawidolesko21@gmail.com>
2023-03-23 10:01:37 +0000

    removed legacy code

--- Webcam.py
+++ Webcam.py
@@ -59,28 +59,4 @@ cv2.destroyWindow("ROI")
 
 
 
-"""     edges = cv2.Canny(gray, canny_min_thresh, canny_max_thresh, apertureSize=aperture_size)
-
-    # Extract ROI based on edges
-    contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    if len(contours) > 0:
-        contour = max(contours, key=cv2.contourArea)
-        x, y, w, h = cv2.boundingRect(contour)
-        x -= margin
-        y -= margin
-        w += 2*margin
-        h += 2*margin
-
-        # Make ROI always a square
-        roi_size = max(w, h)
-        x_center = x + w // 2
-        y_center = y + h // 2
-        x = x_center - roi_size // 2
-        y = y_center - roi_size // 2
-        w = h = roi_size
-
-        roi = frame[max(y, 0):min(y+h, frame.shape[0]), max(x, 0):min(x+w, frame.shape[1])]
-        cv2.imshow('ROI', roi) """
-
-
  
