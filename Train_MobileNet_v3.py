1ab8ceea0dbf2dbdcab0a98bb80d195132333617
Dawid Olesko <dawidolesko21@gmail.com>
2023-04-02 20:28:40 +0100

    updated mobileNet code

--- Train_MobileNet_v3.py
+++ Train_MobileNet_v3.py
@@ -1,28 +1,52 @@
 import tensorflow as tf
+from tensorflow import keras
 from tensorflow.keras.applications import MobileNetV3Small
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+# Define the path to the directory containing the dataset
+data_dir = '/path/to/dataset'
 
 # Define the input shape of the images
-input_shape = (224, 224, 3)
+input_shape = (224, 224)
 
-# Load the MobileNetV3 model
-base_model = MobileNetV3Small(input_shape=input_shape, weights='imagenet', include_top=False)
+# Define the batch size and number of epochs for training
+batch_size = 32
+num_epochs = 10
 
-# Freeze the base model layers
-base_model.trainable = False
+# Define the data generators for training, validation, and testing
+train_datagen = ImageDataGenerator(rescale=1./255,
+                                   rotation_range=20,
+                                   width_shift_range=0.2,
+                                   height_shift_range=0.2,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True,
+                                   fill_mode='nearest')
 
-# Define the classification head
-x = tf.keras.layers.GlobalAveragePooling2D()(base_model.output)
-x = tf.keras.layers.Dense(128, activation='relu')(x)
-x = tf.keras.layers.Dense(64, activation='relu')(x)
-outputs = tf.keras.layers.Dense(num_classes, activation='softmax')(x)
+val_datagen = ImageDataGenerator(rescale=1./255)
 
-# Define the final model
-model = tf.keras.models.Model(inputs=base_model.input, outputs=outputs)
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_data = train_datagen.flow_from_directory(data_dir + '/train',
+                                                target_size=input_shape,
+                                                batch_size=batch_size,
+                                                class_mode='categorical')
+
+val_data = val_datagen.flow_from_directory(data_dir + '/val',
+                                            target_size=input_shape,
+                                            batch_size=batch_size,
+                                            class_mode='categorical')
+
+test_data = test_datagen.flow_from_directory(data_dir + '/test',
+                                              target_size=input_shape,
+                                              batch_size=batch_size,
+                                              class_mode='categorical')
+
+# Define the MobileNetV3Small model
+model = MobileNetV3Small(input_shape=input_shape, classes=2)
 
 # Compile the model
-model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
 # Train the model
 history = model.fit(train_data,
@@ -33,4 +57,4 @@ history = model.fit(train_data,
 test_loss, test_acc = model.evaluate(test_data)
 
 # Save the model
-model.save('mobilenetv3.h5')
+model.save('my_model.h5')
