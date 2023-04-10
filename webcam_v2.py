196604ba5479db5887a3c476476418cf343a5121
Dawid Olesko <dawidolesko21@gmail.com>
2023-04-10 17:31:46 +0100

    updated webcamv2

--- webcam_v2.py
+++ webcam_v2.py
@@ -1,65 +1,81 @@
 import cv2
 import numpy as np
+import tensorflow as tf
+import time
 
-# Load YOLOv4-tiny with Darknet
-net = cv2.dnn.readNet('yolov4-tiny.weights', 'yolov4-tiny.cfg')
-classes = []
-with open('coco.names', 'r') as f:
-    classes = [line.strip() for line in f.readlines()]
-layer_names = net.getLayerNames()
-output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+# Load your trained model
+model_path = 'model.h5'
+model = tf.keras.models.load_model(model_path)
 
-# Initialize camera feed
+# Label map
+label_map = {0: 'Cut', 1: 'Dressing', 2: 'F_Body', 3: 'Red_T'}
+
+def process_frame(frame, model, input_size):
+    resized_frame = cv2.resize(frame, input_size)
+    input_data = np.expand_dims(resized_frame, axis=0) / 255.0
+    predictions = model.predict(input_data)
+    return predictions
+
 cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 640)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
+prev_time = 0
 while True:
-    # Read frame from camera feed
     ret, frame = cap.read()
+    key = cv2.waitKey(20)
+    if not ret:
+        break
 
-    # Preprocess the frame for YOLOv4-tiny
-    blob = cv2.dnn.blobFromImage(frame, 1/255, (416, 416), swapRB=True, crop=False)
-    net.setInput(blob)
-    layer_outputs = net.forward(output_layers)
+    # Calculate FPS
+    current_time = time.time()
+    fps = 1 / (current_time - prev_time)
+    prev_time = current_time
 
-    # Process the outputs of YOLOv4-tiny
-    boxes = []
-    confidences = []
-    class_ids = []
-    for output in layer_outputs:
-        for detection in output:
-            scores = detection[5:]
-            class_id = np.argmax(scores)
-            confidence = scores[class_id]
-            if confidence > 0.5:
-                center_x = int(detection[0] * frame.shape[1])
-                center_y = int(detection[1] * frame.shape[0])
-                width = int(detection[2] * frame.shape[1])
-                height = int(detection[3] * frame.shape[0])
-                left = int(center_x - width / 2)
-                top = int(center_y - height / 2)
-                boxes.append([left, top, width, height])
-                confidences.append(float(confidence))
-                class_ids.append(class_id)
+    # Find the largest contour
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    _, thresh = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
+    edges = cv2.Canny(thresh, 400, 600, apertureSize=3)
+    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    largest_contour = None
+    for contour in contours:
+        if largest_contour is None or cv2.contourArea(contour) > cv2.contourArea(largest_contour):
+            largest_contour = contour
 
-    # Apply non-max suppression to remove redundant boxes
-    indices = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+    if largest_contour is not None:
+        x, y, w, h = cv2.boundingRect(largest_contour)
+        max_side = max(w, h)
+        margin = 20
+        roi_x = max(0, x - (max_side - w) // 2 - margin)
+        roi_y = max(0, y - (max_side - h) // 2 - margin)
+        roi_w = min(max_side + 2 * margin, frame.shape[1] - roi_x)
+        roi_h = min(max_side + 2 * margin, frame.shape[0] - roi_y)
 
-    # Draw the boxes on the frame and show the result
-    if len(indices) > 0:
-        for i in indices.flatten():
-            x, y, w, h = boxes[i]
-            label = str(classes[class_ids[i]])
-            confidence = confidences[i]
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0,255,0), 2)
-            cv2.putText(frame, f"{label} {confidence:.2f}", (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 2)
+        roi = frame[roi_y:roi_y+roi_h, roi_x:roi_x+roi_w]
+        cv2.imshow('ROI', roi)
 
-    cv2.imshow("Object Detection", frame)
+        # Process the ROI and get the predictions
+        predictions = process_frame(roi, model, (224, 224))
 
-    # Press 'q' to quit
-    if cv2.waitKey(1) == ord('q'):
+        # Get the detected class and confidence
+        predicted_class = np.argmax(predictions)
+        confidence = np.max(predictions)
+
+        # Display the detected class and confidence on the ROI
+        text = f"{label_map[predicted_class]}: {confidence * 100:.2f}%"
+        cv2.putText(roi, text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+
+        # Display FPS on the frame
+        fps_text = f"FPS: {fps:.2f}"
+        cv2.putText(roi, fps_text, (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+        # Show the ROI
+        cv2.imshow('Object Detection', roi)
+
+    else:
+        continue
+    if key == 27: # exit on ESC
         break
 
 cap.release()
-cv2.destroyAllWindows()
+cv2.destroyWindow("Object Detection")
