c0c5a10098f01d5c10d326dc58b3828403db00e7
Dawid Olesko <dawidolesko21@gmail.com>
2023-04-11 13:00:42 +0100

    added code to test models

---
+++ Test_keras_models.py
@@ -0,0 +1,76 @@
+import numpy as np
+import tensorflow as tf
+from keras.models import load_model
+from sklearn.metrics import classification_report, confusion_matrix
+
+# Load the trained model
+model = load_model('modelv2.h5')
+
+# Define test dataset file
+test_tfrecords = "C:/Dataset_Tensorflow_v3/test/test.tfrecord"
+def load_dataset(tfrecords, input_shape, batch_size, num_classes):
+    def parse_tfrecord(example_proto):
+        feature_description = {
+            'image/encoded': tf.io.FixedLenFeature([], tf.string),
+            'image/object/class/label': tf.io.VarLenFeature(tf.int64),
+            'image/height': tf.io.FixedLenFeature([], tf.int64),
+            'image/width': tf.io.FixedLenFeature([], tf.int64),
+        }
+        example = tf.io.parse_single_example(example_proto, feature_description)
+        image = tf.image.decode_jpeg(example['image/encoded'], channels=3)
+        image = tf.image.resize(image, input_shape[:2])
+        image = tf.cast(image, tf.float32) / 255.
+
+        label = example['image/object/class/label']
+        if label.shape[0] == 0:
+            label = tf.constant([0], dtype=tf.int64)
+        else:
+            label = label.values[0]
+        label = tf.one_hot(label, depth=num_classes)
+
+        return image, label
+
+    dataset = tf.data.TFRecordDataset(tfrecords)
+    dataset = dataset.map(parse_tfrecord, num_parallel_calls=tf.data.AUTOTUNE)
+    dataset = dataset.shuffle(buffer_size=1000)
+    dataset = dataset.batch(batch_size)
+    dataset = dataset.prefetch(buffer_size=tf.data.AUTOTUNE)
+
+    return dataset
+
+def read_label_map(label_map_file):
+    with open(label_map_file, 'r') as f:
+        lines = f.readlines()
+    label_map = {i: class_name.strip() for i, class_name in enumerate(lines)}
+    return label_map
+
+label_map_file = 'C:\Dataset_Tensorflow_v3/label_map.txt'
+label_map = read_label_map(label_map_file)
+
+# Define parameters
+input_shape = (224, 224, 3)
+num_classes = len(label_map)
+batch_size = 32
+
+
+# Load test dataset
+
+test_data = load_dataset(test_tfrecords, input_shape, batch_size, num_classes)
+
+# Get true labels from the test dataset
+y_true = []
+for _, labels in test_data.unbatch():
+    y_true.append(np.argmax(labels.numpy()))
+
+y_true = np.array(y_true)
+
+# Get model predictions on the test dataset
+y_pred = np.argmax(model.predict(test_data), axis=1)
+
+# Calculate and print the classification report
+print("Classification Report:")
+print(classification_report(y_true, y_pred, target_names=list(label_map.values())))
+
+# Calculate and print the confusion matrix
+print("Confusion Matrix:")
+print(confusion_matrix(y_true, y_pred))
---
+++ Train_Mobilenet_v2_TFRecord.py
@@ -0,0 +1,66 @@
+import tensorflow as tf
+from keras.applications import MobileNetV2
+from keras.optimizers import Adam
+
+# Custom load_dataset function
+def load_dataset(tfrecords, input_shape, batch_size, num_classes):
+    def parse_tfrecord(example_proto):
+        feature_description = {
+            'image/encoded': tf.io.FixedLenFeature([], tf.string),
+            'image/object/class/label': tf.io.VarLenFeature(tf.int64),
+            'image/height': tf.io.FixedLenFeature([], tf.int64),
+            'image/width': tf.io.FixedLenFeature([], tf.int64),
+        }
+        example = tf.io.parse_single_example(example_proto, feature_description)
+        image = tf.image.decode_jpeg(example['image/encoded'], channels=3)
+        image = tf.image.resize(image, input_shape[:2])
+        image = tf.cast(image, tf.float32) / 255.
+
+        label = example['image/object/class/label']
+        if label.shape[0] == 0:
+            label = tf.constant([0], dtype=tf.int64)
+        else:
+            label = label.values[0]
+        label = tf.one_hot(label, depth=num_classes)
+
+        return image, label
+
+    dataset = tf.data.TFRecordDataset(tfrecords)
+    dataset = dataset.map(parse_tfrecord, num_parallel_calls=tf.data.AUTOTUNE)
+    dataset = dataset.shuffle(buffer_size=1000)
+    dataset = dataset.batch(batch_size)
+    dataset = dataset.prefetch(buffer_size=tf.data.AUTOTUNE)
+
+    return dataset
+
+def read_label_map(label_map_file):
+    with open(label_map_file, 'r') as f:
+        lines = f.readlines()
+    label_map = {i: class_name.strip() for i, class_name in enumerate(lines)}
+    return label_map
+
+label_map_file = 'C:\Dataset_Tensorflow_v3/label_map.txt'
+label_map = read_label_map(label_map_file)
+
+# Define parameters
+input_shape = (224, 224, 3)
+num_classes = len(label_map)
+batch_size = 32
+num_epochs = 100
+train_tfrecords = "C:/Dataset_Tensorflow_v3/train/train.tfrecord"
+val_tfrecords = "C:/Dataset_Tensorflow_v3/valid/val.tfrecord"
+
+# Load datasets
+train_data = load_dataset(train_tfrecords, input_shape, batch_size, num_classes)
+val_data = load_dataset(val_tfrecords, input_shape, batch_size, num_classes)
+
+# Create and compile the model
+base_model = MobileNetV2(input_shape=input_shape, include_top=False, weights='imagenet', pooling='avg')
+x = tf.keras.layers.Dense(num_classes, activation='softmax')(base_model.output)
+model = tf.keras.Model(inputs=base_model.inputs, outputs=x)
+
+model.compile(optimizer=Adam(learning_rate=1e-4), loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Train the model
+history = model.fit(train_data, epochs=num_epochs, validation_data=val_data)
+model.save('model_mobilev2.h5')
