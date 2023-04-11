d22d620b2adcff100025fb518826386b61367b30
Dawid Olesko <dawidolesko21@gmail.com>
2023-04-11 20:53:54 +0100

    further changes to test code

--- Test_keras_models.py
+++ Test_keras_models.py
@@ -9,7 +9,44 @@ model = load_model('model_mobilev2_v3.h5')
 # Define test dataset file
 test_tfrecords = "C:/Dataset_Tensorflow_v5/test/test.tfrecord"
 
-# ... (keep the load_dataset and read_label_map functions here)
+def load_dataset(tfrecords, input_shape, batch_size, num_classes):
+    def parse_tfrecord(example_proto):
+        feature_description = {
+            'image/encoded': tf.io.FixedLenFeature([], tf.string),
+            'image/object/class/label': tf.io.VarLenFeature(tf.int64),
+            'image/height': tf.io.FixedLenFeature([], tf.int64),
+            'image/width': tf.io.FixedLenFeature([], tf.int64),
+        }
+        example = tf.io.parse_single_example(example_proto, feature_description)
+        image = tf.image.decode_jpeg(example['image/encoded'], channels=3)
+        image = tf.image.resize(image, input_shape[:2])
+        image = tf.cast(image, tf.float32) / 255.
+
+        label = example['image/object/class/label']
+        if label.shape[0] == 0:
+            label = tf.constant([0], dtype=tf.int64)
+        else:
+            label = label.values[0]
+        label = tf.one_hot(label, depth=num_classes)
+
+        return image, label
+
+    dataset = tf.data.TFRecordDataset(tfrecords)
+    dataset = dataset.map(parse_tfrecord, num_parallel_calls=tf.data.AUTOTUNE)
+    dataset = dataset.shuffle(buffer_size=1000)
+    dataset = dataset.batch(batch_size)
+    dataset = dataset.prefetch(buffer_size=tf.data.AUTOTUNE)
+
+    return dataset
+
+def read_label_map(label_map_file):
+    with open(label_map_file, 'r') as f:
+        lines = f.readlines()
+    label_map = {i: class_name.strip() for i, class_name in enumerate(lines)}
+    return label_map
+
+label_map_file = 'C:\Dataset_Tensorflow_v5/label_map.txt'
+label_map = read_label_map(label_map_file)
 
 # Define parameters
 input_shape = (224, 224, 3)
